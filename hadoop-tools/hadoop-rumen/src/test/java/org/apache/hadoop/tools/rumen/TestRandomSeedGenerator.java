ae8760b9cbaa84956e3c907a9f8378b03ca91cd4
Mohamed Khalefa <m.elbasiouny@gmail.com>
2014-01-12 10:28:04 +0200

    a

--- hadoop-tools/hadoop-rumen/src/test/java/org/apache/hadoop/tools/rumen/TestRandomSeedGenerator.java
+++ hadoop-tools/hadoop-rumen/src/test/java/org/apache/hadoop/tools/rumen/TestRandomSeedGenerator.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.tools.rumen;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import  org.apache.hadoop.tools.rumen.RandomSeedGenerator;
 import static org.apache.hadoop.tools.rumen.RandomSeedGenerator.getSeed;
 
 public class TestRandomSeedGenerator {
